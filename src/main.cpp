daaff221b31ccf23e943ca9e4734431c256d37d1
Julia <julianorrish04@gmail.com>
2022-10-15 19:49:39 -0400

    documentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 // COMP-10184 – Mohawk College
-// PIR Sensor Test Application
+// Alarm System
 // I, Julia Norrish, 000839030 certify that this material is my original work.  No other person's work has been used without due acknowledgement.
+// 
 // Make the LED blink for 10 seconds when motion is detected. If the button is pressed while the LED is blinking, it is disabled and
 // will not do anything when motion is detected until the button is pressed again. If the button is not pressed while the LED is
 // blinking, the LED will stay on solid.
+// 
+// @author Julia Norrish
+// @id 000839030
+
 #include <Arduino.h>
 // digital input pin definitions
 #define PIN_PIR D5
@@ -14,13 +19,22 @@
 #define ALARM_ENABLE    1 
 #define ALARM_COUNTDOWN 2 
 #define ALARM_ACTIVE    3 
+
+// define the delay for the blinking LED 
+#define BLINK_DELAY 125
  
+// the state the alarm is in
 int iAlarmState; 
-int buttonState;
-int oldButtonState;
+// the state the button is in
+int ibuttonState;
+// the state of the button from the last loop
+int ioldButtonState;
+// whether or not motion is detected from the PIR sensor
 bool bPIR;
 
-
+// *************************************************************
+// This function checks the state of the alarm. It switches between states depending on
+// if it detects motion and if the button is pressed.
 void checkAlarmState() {
   switch(iAlarmState) {
     case 0:
@@ -28,10 +42,11 @@ void checkAlarmState() {
       digitalWrite(LED_BUILTIN, 1);
 
       // check if the button is pressed
-      if (buttonState != oldButtonState){
-        oldButtonState = buttonState;
+      if (ibuttonState != ioldButtonState){
+        ioldButtonState = ibuttonState;
   
-        if (buttonState == 1){
+        // if the button is pressed, change the alarm state to 1 to wait for motion
+        if (ibuttonState == 1){
           iAlarmState = 1;
         }
     }
@@ -40,21 +55,23 @@ void checkAlarmState() {
       // read the PIR sensor
       bPIR = digitalRead(PIN_PIR);
 
-      // if motion is detected, set the alarm state to 2
+      // if motion is detected, set the alarm state to 2 to start the countdown
       if (bPIR) {
         iAlarmState = 2;
       }
       break;
     case 2:
-
+      // blink the LED 40 times
       for (int i = 0; i <= 40; i ++){
-        buttonState = digitalRead(PIN_BUTTON);
+        // read the button
+        ibuttonState = digitalRead(PIN_BUTTON);
         // check if the button has been pressed
-        if (buttonState != oldButtonState) {
+        if (ibuttonState != ioldButtonState) {
           // save the old state
-          oldButtonState = buttonState;
+          ioldButtonState = ibuttonState;
 
-          if (buttonState == 1){
+          // if the button has been pressed, turn the LED off and set the alarm state to 0
+          if (ibuttonState == 1){
             digitalWrite(LED_BUILTIN, 1);
             iAlarmState = 0;
             break;
@@ -62,9 +79,9 @@ void checkAlarmState() {
         }
         // turn the LED on and off with a short delay to make it blink
         digitalWrite(LED_BUILTIN, LOW);
-        delay(125);
+        delay(BLINK_DELAY);
         digitalWrite(LED_BUILTIN, HIGH);
-        delay(125);
+        delay(BLINK_DELAY);
 
         // if the LED has blinked for 10 seconds, set the alarm state to 3
         if (i == 40){
@@ -94,7 +111,8 @@ void setup() {
 }
 // *************************************************************
 void loop() {
-  buttonState = digitalRead(PIN_BUTTON);
+  // read the button
+  ibuttonState = digitalRead(PIN_BUTTON);
 
   checkAlarmState();  
 }
